eae3612bffce7603fef1ad4992db9ffc1d83639b
tjd03 <44816049+tjd03@users.noreply.github.com>
2019-07-27 11:18:57 -0400

    Original Test
    This was the first commit to origin to get a website

---
+++ scripts/materialize.js
@@ -0,0 +1,16 @@
+// setup materialize components
+document.addEventListener('DOMContentLoaded', function() {
+
+  //initialize Side Navigation Bar
+  var sideNavBar = document.querySelectorAll('.sidenav');
+  M.Sidenav.init(sideNavBar);
+
+  //initialize Dropdown Trigger
+  var dropdown = document.querySelectorAll('.dropdown-trigger');
+  M.Dropdown.init(dropdown);
+
+  //initialize Modal Trigger
+  var modal = document.querySelectorAll('.modal');
+  M.Modal.init(modal); 
+
+});
