35ea69b2e391d6b87c02f265f14c8ae5e96faa66
tjd03 <44816049+tjd03@users.noreply.github.com>
2019-09-02 14:35:23 -0400

    Seventh Commit To The Master For The Website Section Content Section Content
    This is the seventh commit to the master section content section content it is for the webpage and website

---
+++ categories/scripts/materialize.js
@@ -0,0 +1,24 @@
+// setup Home materialize components
+document.addEventListener('DOMContentLoaded', function() {
+
+  //initialize Side Navigation Bar
+  var sideNavBar = document.querySelectorAll('.sidenav');
+  M.Sidenav.init(sideNavBar);
+
+  //initialize Dropdown Trigger
+  var dropdown = document.querySelectorAll('.dropdown-trigger');
+  M.Dropdown.init(dropdown);
+
+  //initialize Modal Trigger
+  var modal = document.querySelectorAll('.modal');
+  M.Modal.init(modal);
+
+  //initialize Floating Action Button Trigger
+  var floatingActionButton = document.querySelectorAll('.fixed-action-btn');
+  M.FloatingActionButton.init(floatingActionButton);
+
+  //initialize Scrollspy Trigger
+  var scrollSpy = document.querySelectorAll('.scrollspy');
+  M.ScrollSpy.init(scrollSpy);
+
+}); 
